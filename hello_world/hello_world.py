abb13eb322122383f8c2187547155f4a30cb8f83
Bentouati Mehdi <mehdi.bentouati76@gmail.com>
2023-04-06 22:34:22 +0200

    First ebpf program

---
+++ hello_world/hello_world.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from bcc import BPF
+
+program = r"""
+int hello(void *ctx) {
+        bpf_trace_printk("Hello world!");
+        return 0;
+}
+"""
+
+b = BPF(text=program)
+syscall = b.get_syscall_fnname("execve")
+b.attach_kprobe(event=syscall, fn_name="hello")
+
+b.trace_print()
